314b5d4084603c0a4308c8a3c82b7b7607a679af
Ray Munoz <rachel@moonrocks.dev>
2024-12-18 10:22:04 -0800

    BD-29: Add documents controller for UI, move unversioned controller to api namespace, add views

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -4,62 +4,16 @@ class DocumentsController < ApplicationController
 
   def index
     @documents = Document.all
-
-    if @documents.any?
-      render json: serialized_resources
-    else
-        render json: {
-            "errors": [
-              {
-                "status": 404,
-                "source": { "pointer": "/documents" },
-                "detail": "No documents found"
-              }
-            ]
-          }.to_json, status: :not_found
-    end
   end
 
   def show
-    if @document
-      render json: DocumentSerializer.new(@document).serializable_hash.to_json
-    else
-      render json: {
-        "errors": [
-          {
-            "status": 404,
-            "source": { "pointer": "/documents/#{document_params[:id]}" },
-            "detail": "Document not found"
-          }
-        ]
-      }.to_json, status: :not_found
-    end
   end
 
   def create
-    if document_creator[:success]
-      render json: DocumentSerializer.new(document_creator_result[:record]).serializable_hash.to_json, status: :created
-    else
-      render json: {
-        "errors": [
-          {
-            "status": 422,
-            "source": { "pointer": "/documents" },
-            "detail": document_creator_result[:error].message
-          }
-        ]
-      }.to_json, status: :unprocessable_entity
-    end
   end
 
   private
   attr_accessor :document_creator_result
-
-  def serialized_resources
-    options = {}
-    options[:is_collection] = true
-    DocumentSerializer.new(@documents, options).serializable_hash.to_json
-  end
 
   def set_document
     @document ||= Document.friendly.find(document_params[:id], allow_nil: true)
