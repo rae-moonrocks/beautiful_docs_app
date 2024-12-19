5224a99635843f1f80f379540b30e34ad84b3d74
Ray Munoz <rachel@moonrocks.dev>
2024-12-19 14:33:06 -0800

    BD-30: Add create functionality, document model validations

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,16 @@ class Document < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  validates :url, presence: true, format: { with: URI.regexp }, uniqueness: true, if: :url_present?
+  validates :title, uniqueness: true
+
   def contributor_name
     contributor.blank? ? "Unknown" : contributor
+  end
+
+  private
+
+  def url_present?
+    url.present?
   end
 end
