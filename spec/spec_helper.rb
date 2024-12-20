b687638b5931d9bb6031d3b45224864c639f434b
Ray Munoz <rachel@moonrocks.dev>
2024-12-20 09:14:21 -0800

    BD-48: Add groups and filters for opencov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,14 @@
 # it.
 #
 require "simplecov"
-SimpleCov.start
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/config/"
+  add_group "Models", "app/models"
+  add_group "Controllers", "app/controllers"
+  add_group "Sidekiq", "app/sidekiq"
+  add_group "Commands", "app/commands"
+end
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
